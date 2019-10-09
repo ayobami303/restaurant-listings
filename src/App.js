3f25878cc55059367abffd51960c33beeae592d7
Omotosho Ayobami <ayobamimoses1@gmail.com>
2019-10-09 10:52:16 +0100

    Revert "App checkbox"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import Button from './component/common/button';
-import Input from './component/common/input';
-import Checkbox from './component/common/checkbox';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return(
-        <div className="App container">
-            Hello world
-            <Button/>
-            <Input/>
-            <Checkbox />
-        </div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
