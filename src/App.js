b500a345be15cee1c023d7c2d40f9e56baa9acfe
Ezenwankwo <ezenwankwogabriel@gmail.com>
2019-10-11 11:09:27 +0100

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 // import Button from './component/common/button';
 // import Input from './component/common/input';
 // import Checkbox from './component/common/checkbox';
@@ -6,16 +11,17 @@ import React from 'react';
 // import Label from './component/common/Label';
 import RestaurantListings from './component/RestaurantListing';
 
-
 function App() {
     return(
-        <div className="App container">
-            <RestaurantListings />
-            {/* <Button/>
-            <Input/>
-            <Checkbox />
-            <Dropdown /> */}
-        </div>
+      <Router>
+        <Switch>
+          <Route path="/">
+          <div className="App container">
+              <RestaurantListings />
+          </div>
+          </Route>
+        </Switch>
+      </Router>
 	);
 }
 
